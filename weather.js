fccaed48ddbc5519ec8c0a7f21c963b201295b38
VSK2003 <vrushalikudande8@gmail.com>
2023-12-17 23:04:12 +0530

    js

--- weather.js
+++ weather.js
@@ -1,1 +1,14 @@
-https://api.openweathermap.org/data/2.5/weather?q=japan&appid=d82e6c7cf22b1d7b5667bfa812af5b77
+// https://api.openweathermap.org/data/2.5/weather?q=japan&appid=d82e6c7cf22b1d7b5667bfa812af5b77
+const apiKey = "d82e6c7cf22b1d7b5667bfa812af5b77"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=japan"
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data= await response.json();
+    console.log(data);
+    document.querySelector(".temp").innerHTML=  Math.round(data.main.temp) + "°C";
+    document.querySelector(".cityName").innerHTML= data.name;
+    document.querySelector(".humidity").innerHTML=data.main.humidity + "%" ;
+    document.querySelector(".wind").innerHTML=data.wind.speed + "km/h"
+}
+checkWeather();
